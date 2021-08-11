5ad6a46d1a05419aea921131ba0514a99e11fa5b
Diego <diegomenecheli@hotmail.com>
2021-08-11 20:09:54 -0300

    add: connect to api

---
+++ app/src/main/java/com/boticario/network/NewsApi.kt
@@ -0,0 +1,12 @@
+package com.boticario.network
+
+import com.boticario.model.news.NewsResponse
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface NewsApi {
+
+    //Get news from api
+    @GET("data.json")
+    suspend fun getNews(): Response<NewsResponse>
+}
--- app/src/main/java/com/boticario/utils/Constants.kt
+++ app/src/main/java/com/boticario/utils/Constants.kt
@@ -2,6 +2,6 @@ package com.boticario.utils
 
 class Constants {
     companion object {
-        const val BASE_URL = "https://gb-mobile-app-teste.s3.amazonaws.com/data.json ."
+        const val BASE_URL = "https://gb-mobile-app-teste.s3.amazonaws.com/"
     }
 }
